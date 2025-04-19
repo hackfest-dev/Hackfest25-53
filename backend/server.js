eca80fe9c411ac3d3a2a5cd385b72b1d32170213
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-04-19 12:21:15 +0530

    Add Vite to Express CORS

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ if (!fs.existsSync(tmpDir)) {
 }
 
 // Middleware setup
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
